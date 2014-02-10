6688c3409600182a13dc2cc35eebc6549c093071
cristiano.zanca <cristiano.zanca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 23:25:30 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/momapix-image-selector/tags/1.0.1@855177 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/admin/momapix_settings.php
@@ -0,0 +1,26 @@
+<?php
+
+   
+include 'api_setup.php';
+	
+?>
+
+
+<form id="momapix_form_data" method="post" action="#">
+
+
+<div id="momapix-general2" class="wrap">
+<!--
+    <h2>Momapix Settings </h2>
+-->
+
+<input type="hidden" name="action"
+value="save_momapix_default_value" />
+<!-- Adding security through hidden referrer field -->
+<?php wp_nonce_field( 'momapix2' ); ?>
+Account Momapix Url: <input type="text" name="momapix_account" value="<?php echo esc_html($options['momapix_account']); ?>" size="55"/><br />
+Account API Key: <input type="text" name="momapix_api_key" value="<?php echo esc_html($options['momapix_api_key']); ?>" size="55"/><br />
+
+<input type="submit" value="Submit" class="button-primary"/>
+</form>
+</div>
---
+++ trunk/js/wp-momapix-gallery.js
@@ -0,0 +1,22 @@
+
+                        function insert_picture(momaurl)
+                        {
+				
+				string = '<a target="_blank" href="' + momaurl + '"><img src="' + momaurl + '" /></a>';
+				
+				var win = window.dialogArguments || opener || parent || top;
+				
+				win.send_to_editor(string);
+      				      				
+      			return true;
+								
+			}
+                        
+  function goBack()
+  {
+  window.history.back()
+  }
+
+    
+
+
