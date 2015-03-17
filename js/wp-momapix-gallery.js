0b5b8647b4f4db2056ae16b494475f1ca6e7985d
cristiano.zanca <cristiano.zanca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-17 10:35:35 +0000

    url image link
    git-svn-id: https://plugins.svn.wordpress.org/momapix-image-selector/trunk@1114483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/wp-momapix-gallery.js
+++ js/wp-momapix-gallery.js
@@ -1,6 +1,6 @@
 function insert_picture($momaurl, $moma2, $date, $location, $title, $credit, $caption, $momaurlitem)
     {
-	string = '<div style="background: none repeat scroll 0 0 white; border: 1px outset gray; color: gray; font-size: 12px; font-style: italic; line-height: 1em; padding: 5px; text-align: justify; width: 100%;"><a target="_blank" href="' + $momaurl + '"><img src="' + $momaurl + '" alt="' + $moma2 + '" /></a>' +
+	string = '<div style="background: none repeat scroll 0 0 white; border: 1px outset gray; color: gray; font-size: 12px; font-style: italic; line-height: 1em; padding: 5px; text-align: justify; width: 100%;"><a target="_blank" href="' + $momaurlitem + '"><img src="' + $momaurl + '" alt="' + $moma2 + '" /></a>' +
 	'<br><p> ' + $date + ' </p><p>' + $location + '' + $title + '</p><p>' + $credit + '</p><p><a href="'+ $momaurlitem +'">' + $momaurlitem + '</a></p><p>' + $caption + '</p> </div>';
 		
 	var win = window.dialogArguments || opener || parent || top;
